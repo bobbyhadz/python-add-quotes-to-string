317824f8c69f790900087dcecf528f478f4d6ce1
bobbyhadz <opmftw@gmail.com>
2024-04-08 16:38:41 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Add quotes to a string in Python
+
+# ✅ Alternating single and double quotes
+result_1 = '"apple"'
+
+# -------------------------------------
+
+# ✅ Add double quotes around a variable
+my_str = 'hello world'
+
+result = f'"{my_str}"'
+
+print(result)  # 👉️ "hello world"
+
+# -------------------------------------
+
+# ✅ Add single quotes around a variable
+my_str = "hello"
+
+result = f"'{my_str}'"
+
+print(result)  # 👉️ 'hello'
+
+# -------------------------------------
+
+# ✅ Escaping double quotes with a backslash
+result_3 = "\"apple\""
